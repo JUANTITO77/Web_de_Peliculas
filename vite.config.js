438df707e2622a4b74aca7d2efd8f2a08e328572
JUANTITO77 <jelvistq77@gmail.com>
2024-06-06 22:07:02 -0500

    Actualizando configuración

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"//JUANTITO77.githup.io/Web_de_Peliculas",
+  base:"https://JUANTITO77.githup.io/Web_de_Peliculas",
 })
